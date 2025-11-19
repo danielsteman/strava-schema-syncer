937d7bb0025106780d9ed3e33a72a7d6b7e26012
Daniel Steman <daniel-steman@live.nl>
2025-11-19 18:04:41 +0100

    feat: add domain

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,12 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
 	app(input) {
 		return {
 			name: 'strava-schema-syncer',
 			removal: input?.stage === 'production' ? 'retain' : 'remove',
 			protect: ['production'].includes(input?.stage),
-			home: 'aws'
+			home: 'aws',
+			providers: { cloudflare: '6.11.0' }
 		};
 	},
 	// deno-lint-ignore require-await
@@ -14,8 +14,11 @@ export default $config({
 		const STRAVA_CLIENT_ID = new sst.Secret('STRAVA_CLIENT_ID');
 		const STRAVA_CLIENT_SECRET = new sst.Secret('STRAVA_CLIENT_SECRET');
 		const STRAVA_REFRESH_TOKEN = new sst.Secret('STRAVA_REFRESH_TOKEN');
-
 		new sst.aws.SvelteKit('StravaSchemaSyncer', {
+			domain: {
+				name: 'running.danielsteman.com',
+				dns: sst.cloudflare.dns()
+			},
 			link: [STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET, STRAVA_REFRESH_TOKEN]
 		});
 	}
