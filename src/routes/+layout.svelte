22f3b3f48274c40849f8d1b58a38b521a77311ae
Daniel Steman <daniel-steman@live.nl>
2025-11-27 15:50:53 +0100

    fix: hash of katex cdn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,7 +44,7 @@
 	<link
 		rel="stylesheet"
 		href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css"
-		integrity="sha384-hGcndoZ8f9JftKct6W/KiDWuszUst8aZ3F2AKa+DrFAKmbcL4ORQGdiT+f47j9g6"
+		integrity="sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+"
 		crossorigin="anonymous"
 	/>
 </svelte:head>
